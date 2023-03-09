202b9a7e9e14e21ce319fa98f10466681946f592
Artus U <artusrock@hotmail.com>
2023-03-09 16:01:03 +0000

    Appointment model

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+# SERVICE_CHOICES = (
+#     ("Consultation", "Consultation"),
+#     ("Mentoring", "Mentoring"),
+# )
+
+TIME_CHOICES = (
+    ("2 PM", "2 PM"),
+    ("2:30 PM", "2:30 PM"),
+    ("3 PM", "3 PM"),
+    ("3:30 PM", "3:30 PM"),
+    ("4 PM", "4 PM"),
+    ("4:30 PM", "4:30 PM"),
+)
+
+
+class Appointment(models.Model):
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    #type = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Consultation")
+    booked = models.BooleanField(default=False)
+    day = models.DateField(null=True, blank=True)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="2 PM")
+
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
